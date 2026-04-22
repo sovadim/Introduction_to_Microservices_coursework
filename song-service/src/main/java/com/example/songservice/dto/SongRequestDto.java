4e381d0ef31d3281833fb2a26f4e8a966a0079c9
Vadim Sofin <Vadim_Sofin@epam.com>
2026-04-22 16:35:30 +0400

    Fix error message when fields are empty

--- song-service/src/main/java/com/example/songservice/dto/SongRequestDto.java
+++ song-service/src/main/java/com/example/songservice/dto/SongRequestDto.java
@@ -12,19 +12,19 @@ public class SongRequestDto {
     @Positive(message = "ID must be a positive number")
     private Integer id;
 
-    @NotBlank(message = "Song name is required")
-    @Size(max = 100, message = "Name must not exceed 100 characters")
+    @NotNull(message = "Song name is required")
+    @Size(min = 1, max = 100, message = "Song name must be between 1 and 100 characters")
     private String name;
 
-    @NotBlank(message = "Artist is required")
-    @Size(max = 100, message = "Artist must not exceed 100 characters")
+    @NotNull(message = "Artist name is required")
+    @Size(min = 1, max = 100, message = "Artist name must be between 1 and 100 characters")
     private String artist;
 
-    @NotBlank(message = "Album is required")
-    @Size(max = 100, message = "Album must not exceed 100 characters")
+    @NotNull(message = "Album name is required")
+    @Size(min = 1, max = 100, message = "Album name must be between 1 and 100 characters")
     private String album;
 
-    @NotBlank(message = "Duration is required")
+    @NotNull(message = "Duration is required")
     @Pattern(regexp = "^\\d{2}:\\d{2}$", message = "Duration must be in mm:ss format with leading zeros")
     private String duration;
 
